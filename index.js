357ce31b4588f4cc679156427df51f05a448c1b6
abi12-me <abirami9028@gmail.com>
2024-08-17 12:51:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+
+var sn=document.querySelector(".side-nav")
+
+function show()
+{
+    sn.style.left="0"
+}
+
+function closenav()
+{
+    sn.style.left="-60%"
+
+}
+
+var eswar=document.querySelector(".bigimage")
+var ol=document.querySelector(".overlay")
+
+function output()
+{
+    eswar.style.display="block"
+    ol.style.display="block"
+
+}
+
+
+function exit()
+{
+    eswar.style.display="none"
+    ol.style.display="none"
+   
+}
